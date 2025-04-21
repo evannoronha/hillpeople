04745126a89ccfe3e881866a6204f5df3cee1aed
Evan Noronha <hello@evannoronha.com>
2025-04-21 11:34:08 -0700

    Strapi Image Upload: Add an xlarge breakpoint

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+    upload: {
+        config: {
+            breakpoints: {
+                xlarge: 1920,
+                large: 1000,
+                medium: 750,
+                small: 500,
+                xsmall: 64
+            },
+        }
+    }
+});
