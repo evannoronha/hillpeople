8958b074414f7bfc5b452c623625d1fdd41859d1
Evan Noronha <hello@evannoronha.com>
2025-04-23 11:45:39 -0700

    Format post view

--- frontend/src/lib/imageUrl.ts
+++ frontend/src/lib/imageUrl.ts
@@ -11,3 +11,17 @@ export const getMediaUrl = (url?: string): string => {
     const baseUrl = STRAPI_URL;
     return `${baseUrl}${url}`;
 };
+
+export const getLargestImage = (post:any) => {
+	const { coverImage } = post;
+	if (!coverImage) return null;
+
+	const formats = coverImage.formats;
+	if (!formats) return null;
+
+	const largestFormat = Object.keys(formats).reduce((largest, key) => {
+		return formats[key].size > formats[largest].size ? key : largest;
+	}, Object.keys(formats)[0]);
+
+	return formats[largestFormat]
+};
