ea33fd9c848bedb67b71ca27d2196a8580c44ab4
Evan Noronha <hello@evannoronha.com>
2025-10-27 13:33:04 -0600

    Inrease media upload size limit

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,6 +1,7 @@
 export default () => ({
     upload: {
         config: {
+            sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
             breakpoints: {
                 xlarge: 1920,
                 large: 1000,
